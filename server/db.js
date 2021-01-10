beb429f764f13120feec5388c33e1bbf65d0f7a6
andre4653 <61825400+andre4653@users.noreply.github.com>
2021-01-10 08:45:56 +0100

    Update db.js

--- server/db.js
+++ server/db.js
@@ -2,7 +2,7 @@ const Pool = require("pg").Pool     //allows to configure(?)
 
 const pool = new Pool({
     user: "postgres",
-    password: "hnpxf8bxf8", 
+    password: "...", 
     host: "localhost",
     port: 5432, 
     database: "jwttutorial"
